40435ba108777dd301f03d330287772dfda97f4a
Ctrelka <eduard.ignatyuk.1998@mail.ru>
2019-02-26 15:35:17 +0300

    create contacts template

---
+++ assets/templates/contacts.js
@@ -0,0 +1,12 @@
+/**
+ * Add Mastak Library
+ */
+import './../mastak-lib/loader/loader';
+/**
+ * Add Common components
+ */
+import './../common/_common.scss';
+import './../components/header/header';
+import './../components/footer/footer';
+import './../components/contacts/contacts';
+import './../components/information/information';
