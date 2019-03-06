9a9b6d3f2d4a304d61ba91ccca2618fa71a5acbe
zankoav <zankoav@mail.ru>
2019-03-06 16:22:31 +0300

    update

--- header.php
+++ header.php
@@ -5,4 +5,4 @@
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<?php wp_head(); ?>
 </head>
-<body class="body_overflow_hidden">
+<body class="body_overflow_hidden <?= is_page_template( 'template-home.php' ) ? 'home-template' : '';?>">
